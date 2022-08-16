ca5f671163e00110ce7dce16a813d56cdea5c5eb
stefangeneralao <stefangeneralao@gmail.com>
2022-08-16 19:57:39 +0200

    Fix faulty dragging

--- app/src/components/Title/index.tsx
+++ app/src/components/Title/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Input, makeStyles } from '@material-ui/core';
+import { Input, makeStyles, Typography } from '@material-ui/core';
 
 interface Props {
   value: string;
@@ -8,12 +8,24 @@ interface Props {
 
 const useStyles = makeStyles({
   input: {
-    fontSize: '1.5rem',
+    lineHeight: 1.5,
+    fontSize: '1.6rem',
+    margin: 0,
+    padding: 0,
+  },
+  default: {
+    lineHeight: 1.5,
+    fontSize: '1.6rem',
+    margin: 0,
+  },
+  root: {
+    padding: 0,
   },
 });
 
 const Title = ({ value: initialValue, onSubmit: onSubmitTitle }: Props) => {
   const [value, setValue] = useState(initialValue);
+  const [isFocus, setIsFocus] = useState(false);
 
   const classes = useStyles();
 
@@ -32,27 +44,36 @@ const Title = ({ value: initialValue, onSubmit: onSubmitTitle }: Props) => {
 
   const onBlur = async () => {
     try {
+      setIsFocus(false);
       await onSubmitTitle(value);
     } catch {
       setValue(initialValue);
     }
   };
 
-  return (
-    <form onSubmit={onSubmit}>
-      <Input
-        className={classes.input}
-        type="text"
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-        disableUnderline
-        placeholder="Add title"
-        multiline
-        fullWidth
-      />
-    </form>
-  );
+  if (isFocus) {
+    return (
+      <form className={classes.root} onSubmit={onSubmit}>
+        <Input
+          className={classes.input}
+          type="text"
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          disableUnderline
+          placeholder="Add title"
+          fullWidth
+          autoFocus
+        />
+      </form>
+    );
+  } else {
+    return (
+      <Typography className={classes.default} onClick={() => setIsFocus(true)}>
+        {value}
+      </Typography>
+    );
+  }
 };
 
 export default Title;
